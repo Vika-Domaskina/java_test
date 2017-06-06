1849c01dac5bf6ea2f40219520a66af11458a97c
Victoria <v1ktor1ya.domask1na@gmail.com>
2017-06-06 17:08:05 +0300

    pochti vse

--- src/com/company/Class_copy.java
+++ src/com/company/Class_copy.java
@@ -1,7 +1,13 @@
 package com.company;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by Viktoriya.D on 6/1/2017.
  */
 public class Class_copy {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -7,19 +7,34 @@ import java.util.Scanner;
  */
 public class Test {
     public static void main(String[] args) {
-        String[] hundreds = new String[]{"", "thousand ", "million "};
+        String[] hundreds = new String[]{"", "thousand ", "million ", "billion"};
         String int_to_string = "";
-        System.out.println("Input number:");
-        Scanner scanner = new Scanner(System.in);
-        String consoleInput = scanner.nextLine();
-        int ochislo = Integer.parseInt(consoleInput);
+        double ochislo;
+        while (true) {
+            System.out.println("Input number:");
+            Scanner scanner = new Scanner(System.in);
+            String consoleInput = scanner.nextLine();
+            try {
+                ochislo = Double.parseDouble(consoleInput);
+                if (ochislo < 0 || ochislo >= 1000000000) {
+                    continue;
+                }
+                    break;
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong number");
+            }
+        }
+        int whole_number = (int) ochislo;
+        //double edfdf = whole_number;
+        double not_whole_number = (int) ((ochislo * 100) - (whole_number * 100));
         int[] chislo = new int[String.valueOf(ochislo).length() / 3 + 1];
         int delitel = 1000, noleuberatel = 1;
-
+        //System.out.println(whole_number);
+        //System.out.println(not_whole_number);
         //System.out.println(chislo);
         for (int i = 0; i < chislo.length; i++) {
-            chislo[i] = (ochislo % delitel) / noleuberatel;
-            ochislo = ochislo - (ochislo % delitel);
+            chislo[i] = (whole_number % delitel) / noleuberatel;
+            whole_number = whole_number - (whole_number % delitel);
             delitel *= 1000;
             noleuberatel *= 1000;
         }
@@ -28,13 +43,21 @@ public class Test {
                 int_to_string = int_to_string + integer_to_text(chislo[i]);
             } else int_to_string = int_to_string + integer_to_text(chislo[i]) + hundreds[i];
         }
+        if ((int) ochislo > 0) {
+
+            int_to_string = int_to_string + "dollars ";
+        }
+        if (not_whole_number > 0) {
+            int_to_string = int_to_string + integer_to_text((int) not_whole_number) + "cents";
+        }
+
         System.out.println(int_to_string);
     }
 
     static String integer_to_text(int a) {
         String[] units = new String[]{"", "one ", "two ", "tree ", "four ", "five ", "six ", "seven ", "eight ", "nine "};
         String[] decimals = new String[]{"", "", "twenty ", "thirty ", "fourty ", "fifty ", "sixty ", "seventy ", " eighty ", "ninety ", "ten ", "eleven ", "twelve ", "thirteen ",
-                "fourteen ", "thirteen ", "thirteen ", "sixteen " , "seventeen ", "eighteen ", "nineteen "};
+                "fourteen ", "thirteen ", "thirteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "};
 
         int ed = a % 10;
         int dec = ((a % 100) - ed) / 10;
